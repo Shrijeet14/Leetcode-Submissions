3cae084ab7f7e924ab2505a04da7c30d93861eed
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-07-05 12:35:47 +0530

    [14] [Time Beats: 28.20%] [Memory Beats: 6.31%] - LeetPush

--- 14-Longest-Common-Prefix.cpp
+++ 14-Longest-Common-Prefix.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+        string ans;
+        int minLengthString = INT_MAX ;
+        string minString =\\;
+        for(int i =0 ; i < strs.size() ; i++){
+            if(strs[i].length() < minLengthString){
+                minLengthString = strs[i].length();
+                minString = strs[i];
+            }
+        }
+
+        for(int i =0 ; i< minLengthString ; i++){
+            int flag = 0;
+
+            for(int j =0 ; j < strs.size()  ; j++ ){
+                string s = strs[j];
+                if(s[i] != minString[i]){
+                    flag = 1;
+                }
+            }
+            if(flag == 0){
+                ans.push_back(minString[i]);
+            }
+            else{
+                return ans;
+            }
+        }
+        return ans;
     }
 };
