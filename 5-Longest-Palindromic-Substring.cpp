835a5998ce728a3f69b49bb1374af101d8040ded
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-07-05 17:30:12 +0530

    [5] [Time Beats: 68.51%] [Memory Beats: 46.33%] - LeetPush

---
+++ 5-Longest-Palindromic-Substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int max_size = 0;
+    string max_string = \\;
+
+    void expander(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            int current_length = right - left + 1;
+            if (current_length > max_size) {
+                max_size = current_length;
+                max_string = s.substr(left, current_length);
+            }
+            left--;
+            right++;
+        }
+    }
+
+    string longestPalindrome(string s) {
+        // max_size = 0; 
+        // max_string = \\; 
+
+        for (int center = 0; center < s.length(); center++) {
+            // Odd length palindromes
+            expander(s, center, center);
+            // Even length palindromes
+            expander(s, center, center + 1);
+        }
+        return max_string;
+    }
+};
