38546a94c6b39f6e559bc8530c3c1ce51cac83f6
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-12-17 00:16:27 +0530

    [141] [Time Beats: 95.79%] [Memory Beats: 17.39%] - LeetPush

--- 141-Linked-List-Cycle.cpp
+++ 141-Linked-List-Cycle.cpp
@@ -8,29 +8,41 @@
  */
  
  // Method- 1 => Using Hashing
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         map<ListNode* , bool> mpp ;
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             if(mpp[temp] == true){
+//                 return true;
+//             }
+//             else{
+//                 mpp[temp] = true;
+//                 temp = temp->next;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+//Method - 2 => Tortoise Algorithm
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        map<ListNode* , bool> mpp ;
-        ListNode* temp = head;
-        while(temp != NULL){
-            if(mpp[temp] == true){
-                return true;
+    bool hasCycle(ListNode *head){
+        ListNode* slow = head ;
+        ListNode* fast = head ;
+
+        while(fast !=NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
             }
-            else{
-                mpp[temp] = true;
-                temp = temp->next;
+            if(fast == slow && fast ){
+                return true;
             }
         }
         return false;
     }
 };
-
-//Method - 2 => Tortoise Algorithm
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-
-        
-//     }
-// };
