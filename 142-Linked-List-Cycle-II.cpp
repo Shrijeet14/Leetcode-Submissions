b6d34b4edc7ef9199b771d20b3f3a4370b79ee22
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-12-17 01:39:12 +0530

    [142] [Time Beats: 98.10%] [Memory Beats: 27.93%] - LeetPush

--- 142-Linked-List-Cycle-II.cpp
+++ 142-Linked-List-Cycle-II.cpp
@@ -31,6 +31,24 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        ListNode* fast = head ;
+        ListNode* slow = head ;
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            if(fast == slow){
+                break;
+            }
+        }
+        if(fast == NULL){return NULL;}
+        slow = head;
+        while(fast != slow){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
 };
