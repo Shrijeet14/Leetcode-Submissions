cb8383d6ce1b00b65d920e5d58d163535e4874ba
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-12-16 07:50:01 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 24.96%] - LeetPush

--- 876-Middle-of-the-Linked-List.cpp
+++ 876-Middle-of-the-Linked-List.cpp
@@ -8,39 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//  Method - 1
+// class Solution {
+// public:
+//     int lengthLL(ListNode* &head){
+//         ListNode* temp = head;
+//         int len = 0;
+//         while(temp != NULL){
+//             len++;
+//             temp = temp->next;
+//         }
+//         return len;
+//     }
+//     ListNode* middleNode(ListNode* head) {
+//         int len = lengthLL(head);
+//         if(len%2 == 0){
+//             len  = len/2;
+//             ListNode* curr = head;
+//             while(len != 1){
+//                 len--;
+//                 curr = curr->next;
+//             }
+//             curr = curr->next;
+//             return curr;
+//         }
+//         else{
+//             len++;
+//             len  = len/2;
+//             ListNode* curr = head;
+//             while(len != 1){
+//                 len--;
+//                 curr = curr->next;
+//             }
+//             return curr;
+//         }
+        
+//     }
+// };
+
+
+
+
+//Method-2 
 class Solution {
 public:
-    int lengthLL(ListNode* &head){
-        ListNode* temp = head;
-        int len = 0;
-        while(temp != NULL){
-            len++;
-            temp = temp->next;
-        }
-        return len;
-    }
     ListNode* middleNode(ListNode* head) {
-        int len = lengthLL(head);
-        if(len%2 == 0){
-            len  = len/2;
-            ListNode* curr = head;
-            while(len != 1){
-                len--;
-                curr = curr->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
             }
-            curr = curr->next;
-            return curr;
         }
-        else{
-            len++;
-            len  = len/2;
-            ListNode* curr = head;
-            while(len != 1){
-                len--;
-                curr = curr->next;
-            }
-            return curr;
-        }
-        
+        return slow;
     }
 };
