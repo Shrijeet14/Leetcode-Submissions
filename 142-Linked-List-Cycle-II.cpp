421d6861d9a82ea94d4a239505e6419ddefdf3e8
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-12-17 01:32:39 +0530

    [142] [Time Beats: 8.19%] [Memory Beats: 11.06%] - LeetPush

--- 142-Linked-List-Cycle-II.cpp
+++ 142-Linked-List-Cycle-II.cpp
@@ -6,20 +6,31 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// Using Mapping Method
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         map<ListNode* , int> mpp;
+//         ListNode* temp = head;
+//         int index = 0;
+//         while(temp != NULL){
+//             if(mpp.find(temp) != mpp.end()){
+//                 return temp;
+//             }
+//             mpp[temp] = index;
+//             index++;
+//             temp = temp->next;
+//         }
+//         return temp; 
+//     }
+// };
+
+
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        map<ListNode* , int> mpp;
-        ListNode* temp = head;
-        int index = 0;
-        while(temp != NULL){
-            if(mpp.find(temp) != mpp.end()){
-                return temp;
-            }
-            mpp[temp] = index;
-            index++;
-            temp = temp->next;
-        }
-        return temp; 
+        
     }
 };
