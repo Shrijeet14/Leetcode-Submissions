ac963c24633ccf7c109123c1921f0953316baf54
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2024-12-19 00:30:18 +0530

    [160] [Time Beats: 74.88%] [Memory Beats: 18.29%] - LeetPush

--- 160-Intersection-of-Two-Linked-Lists.cpp
+++ 160-Intersection-of-Two-Linked-Lists.cpp
@@ -6,23 +6,50 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//Method-1 : Hashing Method
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+//         map<ListNode* , int> mpp;
+//         ListNode* ans= NULL;
+//         while(headA != NULL){
+//             mpp[headA]++;
+//             headA = headA->next;
+//         }
+//         while(headB != NULL){
+//             mpp[headB]++;
+//             if(mpp[headB]==2){
+//                 ans = headB;
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+//         return ans;
+//     }
+// };
+
+
+//Method 2 : Pointer switching Method
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        map<ListNode* , int> mpp;
-        ListNode* ans= NULL;
-        while(headA != NULL){
-            mpp[headA]++;
-            headA = headA->next;
-        }
-        while(headB != NULL){
-            mpp[headB]++;
-            if(mpp[headB]==2){
-                ans = headB;
-                return headB;
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while(tempA != tempB){
+            if(tempA == NULL){
+                tempA = headB;
             }
-            headB = headB->next;
+            else{
+                tempA = tempA->next;
+            }
+            if(tempB == NULL){
+                tempB = headA;
+            }
+            else{
+                tempB = tempB->next;
+            }
         }
-        return ans;
+        return tempA;
     }
 };
