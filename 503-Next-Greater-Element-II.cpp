54f8705365821c133321e0acf4ead97dffa64ca2
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-01-21 22:50:09 +0530

    [503] [Time Beats: 40.09%] [Memory Beats: 18.51%] - LeetPush

--- 503-Next-Greater-Element-II.cpp
+++ 503-Next-Greater-Element-II.cpp
@@ -1,29 +1,26 @@
+
+// Method - 2 : using stack & circular array concept.
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int>ans;
+        int n = nums.size();
         stack <int> st_back;
-        for(int i = nums.size()-1 ; i >=0  ; i--){
-            while(!st_back.empty() && nums[i] >= st_back.top()){
+        for(int i = 2*(n)-1 ; i >=0  ; i--){
+            int i_new = i%n;
+            while(!st_back.empty() && nums[i_new] >= st_back.top()){
                 st_back.pop();
             }
-            if(st_back.empty()){
-                bool flag = false;
-                for(int j = 0 ; j <i ; j++){
-                    if(nums[i] < nums[j]){
-                        ans.push_back(nums[j]);
-                        flag = true;
-                        break;
-                    }
-                }
-                if(flag == false){
+            if(i < n){
+                if(st_back.empty()){
                     ans.push_back(-1);
                 }
+                else{
+                    ans.push_back(st_back.top());
+                }
             }
-            else{
-                ans.push_back(st_back.top());
-            }
-            st_back.push(nums[i]);
+            
+            st_back.push(nums[i_new]);
         }
         reverse(ans.begin() , ans.end());
         return ans;
