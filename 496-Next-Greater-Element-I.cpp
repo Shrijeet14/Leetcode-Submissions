bf46c05b12d3c7bfde4ff22d6cd71dc298b9cf9e
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-01-24 00:53:51 +0530

    [496] [Time Beats: 25.74%] [Memory Beats: 9.75%] - LeetPush

--- 496-Next-Greater-Element-I.cpp
+++ 496-Next-Greater-Element-I.cpp
@@ -26,3 +26,34 @@ public:
     }
 };
 
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> ans(nums2.size(), -1); // Initialize ans vector for next greater elements
+//         stack<int> st;                     // Stack for monotonic decreasing order
+//         map<int, int> mp;                  // Map to store indices of nums2 elements
+
+//         // Populate the ans vector with next greater elements for nums2
+//         for (int i = nums2.size() - 1; i >= 0; i--) {
+//             while (!st.empty() && nums2[i] >= st.top()) {
+//                 st.pop();
+//             }
+//             if (!st.empty()) {
+//                 ans[i] = st.top();
+//             }
+//             st.push(nums2[i]);
+//             mp[nums2[i]] = i; // Store the index of nums2[i]
+//         }
+
+//         vector<int> ansSheet(nums1.size(), -1); // Initialize ansSheet with the size of nums1
+
+//         // Use the map and ans vector to find results for nums1
+//         for (int i = 0; i < nums1.size(); i++) {
+//             ansSheet[i] = ans[mp[nums1[i]]];
+//         }
+
+//         return ansSheet;
+//     }
+// };
+// submitting again
