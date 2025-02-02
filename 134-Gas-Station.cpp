4e6125d540c37fe5c49bae68ce0570fb1b286d0b
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-02-02 15:26:01 +0530

    [134] [Time Beats: 43.28%] [Memory Beats: 72.36%] - LeetPush

---
+++ 134-Gas-Station.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int balance = 0,totalGas = 0, totalCost = 0;
+        int start = 0;
+        
+        for (int i = 0; i < n; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            balance += gas[i] - cost[i];
+            if (balance < 0) {  
+                start = i + 1; 
+                balance = 0;  
+            }
+        }
+        
+        return (totalGas >= totalCost) ? start : -1;
+    }
+};
