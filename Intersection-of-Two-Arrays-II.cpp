6861c2a49860cb406d7ad95d78ee2beec7ecee74
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-05-23 17:40:08 +0530

    [350] [Time Beats: 100.00%] [Memory Beats: 75.84%] - LeetPush

--- Intersection-of-Two-Arrays-II.cpp
+++ Intersection-of-Two-Arrays-II.cpp
@@ -1,30 +1,55 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        //Method-1
+
+        // vector<int> ans;
+        // map<int , int> mp;
+        // if(nums1.size() > nums2.size()){
+        //     for(int i = 0 ; i < nums1.size() ; i++){
+        //         mp[nums1[i]]++;
+        //     }
+        //     for(int i = 0 ; i<nums2.size() ; i++){
+        //         if(mp[nums2[i]] > 0){
+        //             ans.push_back(nums2[i]);
+        //             mp[nums2[i]]--;
+        //         }
+        //     }
+        // }
+        // else{
+        //     for(int i = 0 ; i < nums2.size() ; i++){
+        //         mp[nums2[i]]++;
+        //     }
+        //     for(int i = 0 ; i<nums1.size() ; i++){
+        //         if(mp[nums1[i]] > 0){
+        //             ans.push_back(nums1[i]);
+        //             mp[nums1[i]]--;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //Method-2 
         vector<int> ans;
-        map<int , int> mp;
-        if(nums1.size() > nums2.size()){
-            for(int i = 0 ; i < nums1.size() ; i++){
-                mp[nums1[i]]++;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int f = 0, s = 0;
+        while (f < nums1.size() && s < nums2.size()) {
+            if (nums1[f] == nums2[s]) {
+                ans.push_back(nums1[f]);
+                f++;
+                s++;
             }
-            for(int i = 0 ; i<nums2.size() ; i++){
-                if(mp[nums2[i]] > 0){
-                    ans.push_back(nums2[i]);
-                    mp[nums2[i]]--;
-                }
+            else if (nums1[f] < nums2[s]) {
+                f++;
             }
-        }
-        else{
-            for(int i = 0 ; i < nums2.size() ; i++){
-                mp[nums2[i]]++;
-            }
-            for(int i = 0 ; i<nums1.size() ; i++){
-                if(mp[nums1[i]] > 0){
-                    ans.push_back(nums1[i]);
-                    mp[nums1[i]]--;
-                }
+            else {
+                s++;
             }
         }
+
         return ans;
+
     }
 };
